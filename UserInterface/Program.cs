8d1024f42db33e9fa1f4f3b14e0538aa207ed125
SagharDehghanian <saghardehghanian945@gmail.com>
2024-09-16 16:18:06 +0330

    change main

--- UserInterface/Program.cs
+++ UserInterface/Program.cs
@@ -4,27 +4,35 @@ using Service;
 class Program
 {
     static User user;
+    User admin = new User("Saghar","1384","Admin",200,new List<Item>());
+    static Item item;
+    static string[] dastor2,dastor3;
+    static UserService userService = new UserService();
+    static ItemService itemService = new ItemService();
     static void Main(string[] args)
     {
-        User admin = new User("Saghar","1384","Admin",200,new List<Item>());
-        Item item;
-        string[] dastor2,dastor3;
-        UserService userService = new UserService();
-        ItemService itemService = new ItemService();
+        Item item1 = new Item("Computer",1000);
+        Item item2 = new Item("Pen",8);
+        Item item3 = new Item("Chair",55);
+        itemService.Items = new List<Item>();
+        itemService.Items.Add(item1);
+        itemService.Items.Add(item2);
+        itemService.Items.Add(item3);
+        
         Console.WriteLine("Please enter a number of dastor you want     1.Register     2.Login    exit");
         string dastor = Console.ReadLine();
         while (dastor!="exit")
         {
             switch (dastor)
             {
-                case "1": Console.WriteLine("Please enter your name and your ID ");
+                case "1": Console.WriteLine("Please enter your name and your ID ");//Register
                     string[] dastor1= Console.ReadLine().Split(' ');
                     user = userService.Register(dastor1[0],dastor1[1],"Adi");
                     Console.WriteLine("Please enter a number of dastor you want     1.Login     exit");
                         dastor= Console.ReadLine();
                     switch (dastor)
                         {
-                            case "1":
+                            case "1"://Login
                                 Console.WriteLine("Please enter your name and your ID ");
                                 dastor2 = Console.ReadLine().Split(' ');
                                 bool B = Login(dastor2, userService);
@@ -35,58 +43,51 @@ class Program
                                     {
                                         switch (dastor)
                                         {
-                                            case "1":
+                                            case "1"://Change the information
                                                 Console.WriteLine("Please enter your name, your money and your ID ");
                                                 dastor3 = Console.ReadLine().Split(' ');
-                                                userService.ChangeTheInformation(user, dastor3[0], Convert.ToDouble(dastor3[1]), dastor3[2]);
+                                                user = userService.ChangeTheInformation(user, dastor3[0], Convert.ToDouble(dastor3[1]), dastor3[2]);
                                                 dastor = Console.ReadLine();
                                                 break;
-                                            case "2":
+                                            case "2"://Buy Item
                                                 Console.WriteLine("Please enter your item's name and this price");
                                                 dastor3 = Console.ReadLine().Split(' ');
                                                 item = new Item(dastor3[0], Convert.ToDouble(dastor3[1]));
                                                 Console.WriteLine(itemService.BuyItem(item, user));
                                                 dastor = Console.ReadLine();
                                                 break;
-                                            case "3":
+                                            case "3"://Add Item
                                                 Console.WriteLine("Please enter your item's name and this price");
                                                 dastor3 = Console.ReadLine().Split(' ');
                                                 item = new Item(dastor3[0], Convert.ToDouble(dastor3[1]));
                                                 Console.WriteLine(itemService.Add(item, user));
                                                 dastor = Console.ReadLine();
                                                 break;
-                                            case "4":
+                                            case "4"://Remove Item
                                                 Console.WriteLine("Please enter your item's name and this price");
                                                 dastor3 = Console.ReadLine().Split(' ');
                                                 item = new Item(dastor3[0], Convert.ToDouble(dastor3[1]));
-                                                Console.WriteLine(itemService.Remove(item,
-                                                    userService.Login(dastor2[0], dastor2[1])));
+                                                Console.WriteLine(itemService.Remove(item, userService.Login(dastor2[0], dastor2[1])));
                                                 dastor = Console.ReadLine();
                                                 break;
-                                            case "exit":
-                                                Environment.Exit(1);
+                                            case "exit": Environment.Exit(1);
                                                 break;
-                                            default:
-                                                Console.WriteLine(
-                                                    "Please enter a number of dastor you want     1.Register     2.Login    exit");
+                                            default: Console.WriteLine("Please enter a number of dastor you want     1.Register     2.Login    exit");
                                                 dastor = Console.ReadLine();
                                                 break;
                                         }
                                     }
                                 }
-
                                 break;
-                            case "exit":
-                                Environment.Exit(1);
+                            case "exit": Environment.Exit(1);
                                 break;
                             default:
                                 Console.WriteLine("Please enter a number of dastor you want     1.Login     exit");
                                 dastor = Console.ReadLine();
-                                break;
+                                break; 
                         }
-
                     break;
-                case "2":Console.WriteLine("Please enter your name and your ID "); 
+                case "2":Console.WriteLine("Please enter your name and your ID ");//Login
                     dastor2= Console.ReadLine().Split(' ');
                     bool b=Login(dastor2,userService);
                     if (b)
@@ -99,8 +100,7 @@ class Program
                                 case "1":
                                     Console.WriteLine("Please enter your name, your money and your ID ");
                                     dastor3 = Console.ReadLine().Split(' ');
-                                    userService.ChangeTheInformation(user, dastor3[0], Convert.ToDouble(dastor3[1]),
-                                        dastor3[2]);
+                                    user = userService.ChangeTheInformation(user, dastor3[0], Convert.ToDouble(dastor3[1]), dastor3[2]);
                                     dastor = Console.ReadLine();
                                     break;
                                 case "2":
@@ -124,12 +124,9 @@ class Program
                                     Console.WriteLine(itemService.Remove(item, user));
                                     dastor = Console.ReadLine();
                                     break;
-                                case "exit":
-                                    Environment.Exit(1);
+                                case "exit": Environment.Exit(1);
                                     break;
-                                default:
-                                    Console.WriteLine(
-                                        "Please enter a number of dastor you want     1.Register     2.Login    exit");
+                                default: Console.WriteLine("Please enter a number of dastor you want     1.Register     2.Login    exit");
                                     dastor = Console.ReadLine();
                                     break;
                             }
@@ -145,16 +142,22 @@ class Program
 
     static Boolean Login(string[] dastor,UserService userService)
     {
+        if (dastor[0].Equals("exit") && dastor.Length==1)
+        {
+            Environment.Exit(1);
+        }
         user = userService.Login(dastor[0],dastor[1]);
+        bool userFound = false;
         if (user!=null)
         {
             Console.WriteLine("Please enter a number of dastor you want     1.Change the information    2.Buy Item      3.Add Item     4.Remove Item");
-            return true;
+            userFound = true;
         }
         else
         {
             Console.WriteLine("Error");
-            return false;
+            userFound = false;
         }
+        return userFound;
     }
 }
